dcb2dabb3cec4353adc9fa1fd80efdc679228fe5
C16192 <shu.ishida@chch.ox.ac.uk>
2017-11-21 23:09:10 +0000

    Front page complete except for login.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run()
+    socketio.run(app, debug=True)
