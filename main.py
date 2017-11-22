c8a867870ca1a49c592dc25256d3123f88a42057
C16192 <shu.ishida@chch.ox.ac.uk>
2017-11-22 01:10:01 +0000

    added socketio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -8,6 +8,23 @@ socketio = SocketIO(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/present')
+def present():
+    return render_template('present.html')
+
+
+@app.route('/listen')
+def listen():
+    return render_template('listen.html')
+
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
